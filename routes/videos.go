978f13814334b5a411ab5d8d2f12724898463f2d
Pablo Medina <jmedinamulet@gmail.com>
2023-11-15 12:13:03 -0500

    get all videos endpoint

--- routes/videos.go
+++ routes/videos.go
@@ -3,7 +3,7 @@ package videos
 import (
 	"github.com/gofiber/fiber/v2"
 	"os"
-	// "encoding/json"
+	"encoding/json"
 )
 
 type Video struct {
@@ -19,8 +19,15 @@ type Video struct {
 	Timestamp 	int64 `json:"timestamp"`
 }
 
+type Summary struct {
+	ID 			string `json:"id"`
+	Title 		string `json:"title"`
+	Channel		string `json:"channel"`
+	Image 		string `json:"image"`
+}
+
 func GetVideos(c * fiber.Ctx) error  {
-	file, err := os.Open("./videos.json")
+	file, err := os.Open("./video-details.json")
 	if err != nil {
 		c.Status(500).JSON(fiber.Map{
 			"status": "error",
@@ -30,8 +37,27 @@ func GetVideos(c * fiber.Ctx) error  {
 
 	defer file.Close()
 
+	videos := []Video{}
+
+	decoder := json.NewDecoder((file))
+	if err := decoder.Decode(&videos); err != nil {
+		c.Status((500)).JSON(fiber.Map{
+			"status": "error",
+		})
+	}
+
+	summaries := []Summary{}
+	for _, video:= range videos {
+		summaries = append(summaries, Summary{
+			ID: video.ID,
+			Title: video.Title,
+			Channel: video.Channel,
+			Image: video.Image,
+		})
+	}
+
 	return c.Status(200).JSON(fiber.Map{
 		"status": "success",
-		"message": file,
-	})	
+		"data": summaries,
+	})
 }
